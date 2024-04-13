a68c4bf8a2ce0481fe51b4ef5bed79fdfe37a554
Irvan <142240569+irvan7@users.noreply.github.com>
2024-04-13 21:51:44 +0700

    My commit message

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import logo from "./logo.jpg"; // Adjust the path as necessary
+
+function Header() {
+  return (
+    <header>
+      <img src={logo} alt="Little Lemon Logo" />
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/Main.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Main() {
+  return <main>{/* Add main content here */}</main>;
+}
+
+export default Main;
